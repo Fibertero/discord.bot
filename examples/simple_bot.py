7293eceb789e923e23f349eaf3a8b36208ac197b
Fibertero <85813166+Fibertero@users.noreply.github.com>
2023-04-16 14:03:45 -0300

    Add files via upload
    Signed-off-by: Fibertero <85813166+Fibertero@users.noreply.github.com>

--- examples/simple_bot.py
+++ examples/simple_bot.py
@@ -2,8 +2,8 @@ import os
 from discordbot.bot import ClientApp
 
 TOKEN = os.environ.get('DISCORD_BOSONS_TESTS')
-CLIENT_ID = 1025801029566070824
-GUILD_ID = 898335285719470080
+CLIENT_ID = CLIENT_ID
+GUILD_ID = GUILD_ID
 
 bot = ClientApp(TOKEN, CLIENT_ID)
 
@@ -15,7 +15,7 @@ async def on_ready(bot):
 @bot.slash_command(name="hello", description="Send hello!")
 async def hello(ctx, name:str):
     await ctx.defer(ephemeral=False)
-    await ctx.send(content=f"Hello, {name}!")
+    await ctx.send(content=f"Hello to Discord.bot!")
 
 if __name__ == "__main__":
     bot.run()
--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('requirements.txt') as f:
 
 version = ''
 with open('discordbot/__init__.py') as f:
-	version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', f.read(), re.MULTILINE).group(1)
+	version = re.search(r'^\s*__version__\s*=\s*[\'"]([^\'"]*)[\'"]', f.read(), re.MULTILINE).group(1)
 
 if not version:
 	raise RuntimeError('version is not set')
